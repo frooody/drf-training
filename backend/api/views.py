006a847f0ceaa5ad0be56758e99e1a6d366ab831
frooody <mister.dennisk@gmail.com>
2023-07-28 20:32:59 +0300

    API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,12 @@
 from django.http import JsonResponse
 import json
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    body = request.body
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    #print(request.header)
-    data['headers'] = dict(request.headers)
-    data['params'] = dict(request.GET)
-    data['content_type'] = request.content_type
-    #print(data)
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
